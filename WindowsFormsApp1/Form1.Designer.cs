46bd0a122e15f0781075e9efe4ba95211c7fb4c8
Viktorhjh <Viktorsek7@gmail.com>
2022-01-09 23:49:56 +0100

    Добавьте файлы проекта.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.chooseFolderToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zad1ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zad2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zad3ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.chooseFolderToolStripMenuItem,
+            this.zad1ToolStripMenuItem,
+            this.zad2ToolStripMenuItem,
+            this.zad3ToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // chooseFolderToolStripMenuItem
+            // 
+            this.chooseFolderToolStripMenuItem.Name = "chooseFolderToolStripMenuItem";
+            this.chooseFolderToolStripMenuItem.Size = new System.Drawing.Size(116, 24);
+            this.chooseFolderToolStripMenuItem.Text = "Choose folder";
+            this.chooseFolderToolStripMenuItem.Click += new System.EventHandler(this.chooseFolderToolStripMenuItem_Click);
+            // 
+            // zad1ToolStripMenuItem
+            // 
+            this.zad1ToolStripMenuItem.Name = "zad1ToolStripMenuItem";
+            this.zad1ToolStripMenuItem.Size = new System.Drawing.Size(61, 24);
+            this.zad1ToolStripMenuItem.Text = "Zad 1";
+            this.zad1ToolStripMenuItem.Click += new System.EventHandler(this.zad1ToolStripMenuItem_Click);
+            // 
+            // zad2ToolStripMenuItem
+            // 
+            this.zad2ToolStripMenuItem.Name = "zad2ToolStripMenuItem";
+            this.zad2ToolStripMenuItem.Size = new System.Drawing.Size(61, 24);
+            this.zad2ToolStripMenuItem.Text = "Zad 2";
+            this.zad2ToolStripMenuItem.Click += new System.EventHandler(this.zad2ToolStripMenuItem_Click);
+            // 
+            // zad3ToolStripMenuItem
+            // 
+            this.zad3ToolStripMenuItem.Name = "zad3ToolStripMenuItem";
+            this.zad3ToolStripMenuItem.Size = new System.Drawing.Size(61, 24);
+            this.zad3ToolStripMenuItem.Text = "Zad 3";
+            this.zad3ToolStripMenuItem.Click += new System.EventHandler(this.zad3ToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(12, 98);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(776, 340);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Path: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "File Search";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem chooseFolderToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zad1ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zad2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zad3ToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string path;
+        int max = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chooseFolderToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();            
+            folderBrowserDialog.ShowNewFolderButton = false;
+            if(folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = folderBrowserDialog.SelectedPath;
+                label2.Text = $"Path: {path}";
+            }
+            else
+            {
+                MessageBox.Show("Error");
+            }
+        }
+
+        private void zad1ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            zadanie1(new DirectoryInfo(path));
+        }
+
+        private void zadanie1(DirectoryInfo directoryInfo)
+        {            
+            FileInfo[] fileInfos = directoryInfo.GetFiles("*.*");
+            listBox1.Items.Add($"                   {fileInfos.Length} in folder: {directoryInfo.FullName}:");
+
+            int cur = fileInfos.Length;
+            
+
+            foreach (FileInfo file in fileInfos)
+            {
+                listBox1.Items.Add($"{file.ToString()}, {file.CreationTime}");
+                if (max < cur && file.CreationTime.Year == 2021)
+                {
+                    max = fileInfos.Length;
+                    label1.Text = $"Max files: {max} in {directoryInfo.FullName}";
+                }
+            }
+            DirectoryInfo[] folders = directoryInfo.GetDirectories();
+            foreach(DirectoryInfo directory in folders)
+            {                
+                zadanie1(directory);
+            }
+        }
+
+        private void zad2ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            listBox1.Items.Clear();
+
+            string[] subDirs = Directory.GetFiles(path);
+            foreach (string subdirPath in subDirs)
+            {
+                System.IO.FileInfo fileInfo = new FileInfo(subdirPath);
+                DateTime creationTime = Directory.GetCreationTime(subdirPath);
+                if (creationTime.Year >= 2010 && creationTime.Year <= 2021 && fileInfo.Length > 1000000)
+                {
+                    listBox1.Items.Add($" {subdirPath}, {creationTime.Year}, {fileInfo.Length}");                
+                    count++;
+                }
+            }
+            label1.Text = $"Count: {count}";            
+        }
+
+        private void zad3ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            string pattern = Microsoft.VisualBasic.Interaction.InputBox("Enter pattern: ");            
+            string[] file = Directory.GetFiles(path, $"*.{pattern}", SearchOption.AllDirectories);           
+            label1.Text = $"Count: {file.Length}";
+            foreach(string str in file)
+            {
+                listBox1.Items.Add($"{str}");
+            }
+        }
+    }
+}
